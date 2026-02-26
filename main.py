79a03dcad2b8d819fbf067687b5b580c293703d8
Misha <Misha2009win@yandex.ru>
2026-02-26 21:05:01 +0300

    26.02.2026

--- main.py
+++ main.py
@@ -3,25 +3,37 @@ from functools import wraps
 import subprocess
 import json
 import os
+import uuid
+import threading
+import time
 
 app = Flask(__name__)
 app.secret_key = 'qhtgyuj12cAv0.'
-users_data = dict() 
+users_data = dict()
 overwrite_users_data = True #Перезаписывает данные для всех пользователей тоесть обнуляет их
 
 def init(): #Делаем инициацию данных
     global users_data
     if not os.path.exists('test'):
         os.mkdir('test')
-    if not os.path.exists('users'):
-        os.mkdir('users')
-    with open('users_data.json') as file: #Получаем данные пользователей
-        users_data = json.load(file)
-    for name in users_data:
-        path = 'users/' + name + '.json'
-        if (not os.path.exists(path)) or overwrite_users_data:
-            with open(path, 'w') as file:
-                json.dump(dict(), file)
+    if not os.path.exists('users_data.json'):
+        with open('users_data.json', 'w') as file:
+            json.dump(dict(), file)
+    with open('users.json') as users_json, open('users_data.json', 'r') as users_data_json:
+        users = json.load(users_json)
+        if overwrite_users_data:
+            for username in users:
+                users_data[username] = dict()
+                users_data[username]['password'] = users[username]
+        else:
+            users_data = json.load(users_data_json)
+    print(users_data)
+
+def save_users_data():
+    while True:
+        time.sleep(15)
+        with open('users_data.json', 'w') as file:
+            json.dump(users_data, file, indent=2)
 
 def login_required(func): #Декоративная функция для требования авторизации
     @wraps(func)
@@ -43,7 +55,7 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        if (username not in users_data) or (users_data[username] != password):
+        if (username not in users_data) or (users_data[username]['password'] != password):
             return render_template('login.html', error = 'Неправильное имя или пароль')
         else:
             session['username'] = username
@@ -68,15 +80,13 @@ def contest(contest):
         abort(404)
     with open('tasks/' + contest + '/' + 'condition_contest.json', 'r') as file:
         condition_contest = json.load(file)
-    with open('users/' + username + '.json', 'r') as file:
-        user = json.load(file)
     return render_template('contest.html',
     contest_name = condition_contest['contest_name'], contest_info = condition_contest['contest_info'],
-    A_name = condition_contest.get('A_name', ''), A_info = condition_contest.get('A_info', ''), A_ref = condition_contest.get('A_ref', ''), A_score = user.get(contest + 'A', 0),
-    B_name = condition_contest.get('B_name', ''), B_info = condition_contest.get('B_info', ''), B_ref = condition_contest.get('B_ref', ''), B_score = user.get(contest + 'B', 0),
-    C_name = condition_contest.get('C_name', ''), C_info = condition_contest.get('C_info', ''), C_ref = condition_contest.get('C_ref', ''), C_score = user.get(contest + 'C', 0),
-    D_name = condition_contest.get('D_name', ''), D_info = condition_contest.get('D_info', ''), D_ref = condition_contest.get('D_ref', ''), D_score = user.get(contest + 'D', 0),
-    E_name = condition_contest.get('E_name', ''), E_info = condition_contest.get('E_info', ''), E_ref = condition_contest.get('E_ref', ''), E_score = user.get(contest + 'E', 0)
+    A_name = condition_contest.get('A_name', ''), A_info = condition_contest.get('A_info', ''), A_ref = condition_contest.get('A_ref', ''), A_score = users_data[username].get(contest + 'A', 0),
+    B_name = condition_contest.get('B_name', ''), B_info = condition_contest.get('B_info', ''), B_ref = condition_contest.get('B_ref', ''), B_score = users_data[username].get(contest + 'B', 0),
+    C_name = condition_contest.get('C_name', ''), C_info = condition_contest.get('C_info', ''), C_ref = condition_contest.get('C_ref', ''), C_score = users_data[username].get(contest + 'C', 0),
+    D_name = condition_contest.get('D_name', ''), D_info = condition_contest.get('D_info', ''), D_ref = condition_contest.get('D_ref', ''), D_score = users_data[username].get(contest + 'D', 0),
+    E_name = condition_contest.get('E_name', ''), E_info = condition_contest.get('E_info', ''), E_ref = condition_contest.get('E_ref', ''), E_score = users_data[username].get(contest + 'E', 0)
     )
 
 @app.route('/<contest>/<num>') #Функция для возврата страницы задачи
@@ -85,11 +95,8 @@ def task(contest, num):
     if not os.path.exists('tasks/' + contest + '/' + num + '/' + 'condition_task.json'):
         abort(404)
     username = str(session.get('username'))
-    path_user = 'users/' + username + '.json'
     path_condition = 'tasks/' + contest + '/' + num + '/' + 'condition_task.json'
     path_condition_long = 'tasks/' + contest + '/' + num + '/' + 'condition_task_long.txt'
-    with open(path_user, 'r') as file:
-        user = json.load(file)
     with open(path_condition, 'r') as condition:
         condition_task = json.load(condition)
     with open(path_condition_long, 'r') as condition_long:
@@ -103,7 +110,7 @@ def task(contest, num):
                            sample_out_1 = condition_task.get('sample_out_1', ''),
                            sample_in_2 = condition_task.get('sample_in_2', ''),
                            sample_out_2 = condition_task.get('sample_out_2', ''),
-                           is_solved = user.get(contest + num, '0'),
+                           is_solved = users_data[username].get(contest + num, '0'),
                            contest = contest,
                            num = num
                            )
@@ -111,22 +118,19 @@ def task(contest, num):
 @app.route('/check', methods=['POST']) #Функция для проверки решения пользователей
 @login_required
 def check():
+    id = uuid.uuid4() #генерирует случайны id для поссылки пользователя
     username = str(session.get('username'))
-    with open('users/' + username + '.json', 'r') as file:
-        user = json.load(file)
     code = str(request.form.get('code'))
     language = str(request.form.get('lang'))
     contest = str(request.form.get('contest'))
     num = str(request.form.get('num'))
     path = 'tasks/' + contest + '/' + num + '/'
-    with open(f'test/{username}.{language}', 'w') as file:
+    with open(f'test/{id}.{language}', 'w') as file:
         file.write(code)
-    res = subprocess.run(['python3', 'checker.py'], input=f"{language}\n{path}\n{username}\n{code}", text=True, capture_output=True)
+    res = subprocess.run(['python3', 'checker.py'], input=f"{language}\n{path}\n{id}", text=True, capture_output=True)
     out = res.stdout
-    if out.split()[0].isdigit():
-        user[contest + num] = max(user.get(contest + num, 0), int(out.split()[0]))
-    with open('users/' + username + '.json', 'w') as file:
-        json.dump(user, file)
+    if out.split()[0].isdigit(): #Проверка что с проверки пришли баллы а не ошибка
+        users_data[username][contest + num] = max(users_data[username].get(contest + num, 0), int(out.split()[0]))
     return out
 
 
@@ -134,5 +138,7 @@ def check():
 
 if __name__ == "__main__":
     init()
+    thread = threading.Thread(target=save_users_data, daemon=True)
+    thread.start()
     app.run(debug=True, host='0.0.0.0') #Если стоит дебаг то код на питоне не работает
     #host = '0.0.0.0' позваляет достучаться из локальной сети
